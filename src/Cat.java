48856cb3c2b69c3328a2e6dbc29a724fc66ca8ab
Stephen <stephen@umw.edu>
2025-10-20 12:21:55 -0400

    Initial commit. Create Cat and Hairball classes.

---
+++ src/Cat.java
@@ -0,0 +1,11 @@
+
+import java.util.ArrayList;
+
+public class Cat {
+    private String color;
+    private ArrayList<Hairball> myHairballs;
+    public static void main(String args[]) {
+        java.util.Scanner input = new java.util.Scanner(System.in);
+        
+    }
+}
---
+++ src/Hairball.java
@@ -0,0 +1,3 @@
+public class Hairball {
+    private int sizeInLbs;
+}
