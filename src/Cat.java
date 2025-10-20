2db25b52725fb828f94df0e94f5cc74eb1d45548
Stephen <stephen@umw.edu>
2025-10-20 12:29:22 -0400

    Add Cat constructor.

--- src/Cat.java
+++ src/Cat.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 public class Cat {
     private String color;
     private ArrayList<Hairball> myHairballs;
+
+    Cat() {
+        myHairballs = new ArrayList<Hairball>();
+    }
     public static void main(String args[]) {
         java.util.Scanner input = new java.util.Scanner(System.in);
         
